31264095b377b90cbc5caf22f0ee59821b7af31c
Guilherme Azevedo <guilhermeazevedo.deb@gmail.com>
2024-11-23 19:09:42 -0300

    feat: get cart by user ID.

--- internal/repository/cart_repository.go
+++ internal/repository/cart_repository.go
@@ -2,11 +2,13 @@ package repository
 
 import (
 	"github.com/azevedoguigo/florindas-acessorios-api/internal/domain"
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
 type CartRepository interface {
 	Create(cart *domain.Cart) error
+	FindByUserID(userID uuid.UUID) (*domain.Cart, error)
 }
 
 type cartRepository struct {
@@ -19,4 +21,14 @@ func NewCartRepository(db *gorm.DB) CartRepository {
 
 func (r cartRepository) Create(cart *domain.Cart) error {
 	return r.db.Create(cart).Error
+}
+
+func (r cartRepository) FindByUserID(userID uuid.UUID) (*domain.Cart, error) {
+	cart := domain.Cart{UserID: userID}
+
+	if err := r.db.First(&cart).Error; err != nil {
+		return nil, err
+	}
+
+	return &cart, nil
 }
